76c2fa3cfef9928fa63b537585e2c03bcd05f4e7
Ayush Kumar <115494316+AyushKumar002@users.noreply.github.com>
2025-04-12 16:53:39 +0530

    Modified the Comments, Method Overloading , Riding ,Final , Static

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@
 class Keyboard {
     private int keys;
     String Colors;
-    public Keyboard() {
-                         // when we create the constructor when the object is called it directly takes the value from here
-        Colors = "Red";
-    }// these are the instance varibales that are Stored with the object
 
-    public void setkey(int keys){
-    this.keys = keys;
-    }
+
     public void pressed(){
         System.out.println("Pressed!");
 
@@ -22,21 +16,38 @@ class Keyboard {
         keys = 18;
     }
 }
+class AdvKeyborad extends Keyboard {
+    public void unPressed(){
+        System.out.println("Unpressed!");
+    }
+    public void pressed(){
+        System.out.println("Pressed by AdvKEYBOARD !");
+
+    }
+}
 
 
 // final constant value dont change it is thougout the code
 //ambuguity issue in the java for the mutliple inheritance so thats why they have removed it
-
-
+//METHOD OVERLOADING( inside the same class) - here we can have the same method name with the differnet parameter or the other one cannot have it
+//POLYMORHPHISM - samething with the differnet behaviour callled the same Method but the object is that it matter of which you have made then its blueprint will be there for it.
+//METHOD OVERRIDING( same method name in different classes here )
+// Abstraction is like hiding of the work we know that we can use the car but its of no use how the engine is working like what kind of pistons it is Using
+// Static Varibales or the Methods can be Direclty be Called by the Classes And shares the single Value as they are stored Elsewhere and we call them wihtout the obj
 public class Main {
     public static void main(String[] args) {
         int num;
         num =8;
 
-        Keyboard keyboardobj = new Keyboard();
+        Keyboard keyboardobj;
+        keyboardobj = new AdvKeyborad();
+
         keyboardobj.pressed();
-        keyboardobj.throwit();
-        System.out.println(keyboardobj.Colors);
+
+
+
+
+
 
     }
 }
